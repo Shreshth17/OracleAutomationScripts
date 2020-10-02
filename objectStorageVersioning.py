95c0cbe76d3a9062c7873839c1679e1451b198db
Shreshth17 <71917978+Shreshth17@users.noreply.github.com>
2020-10-02 14:11:17 +0530

    Create objectStorageVersioning.py

---
+++ objectStorageVersioning.py
@@ -0,0 +1,18 @@
+# This code provides logic to perform auto remediation in oracle, it enables versioning of an object storage bucket
+
+def handler(ctx, data: io.BytesIO = None):
+    body=dict()
+    try:
+        logging.getLogger().info(data)
+        body = json.loads(data.getvalue())
+        logging.getLogger().info(body)
+
+        eventData = body["data"]["additionalDetails"]
+        version= eventData["versioning"]
+        namespace= eventData["namespace"]
+        bucketName= eventData["bucketName"]
+        # config has to be added by the user
+        storageClient = oci.object_storage.ObjectStorageClient(config)
+        if version=='Disabled' or version=='Suspended':
+            response= storageClient.update_bucket(namespace, bucketName, UpdateBucketDetails)
+            print(response)
